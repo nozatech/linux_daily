d622af9a2fca1d96fe0882864c42f155aed4a913
nozatech <albertpark5@gmail.com>
2016-03-29 13:27:26 -0700

    fizzbuzz

---
+++ fizzbuzz.sh
@@ -0,0 +1,60 @@
+FizzBuzz
+
+
+############
+# for loop #
+############
+
+#!/bin/bash
+
+for i in {1..30}; do
+        if (( $i % 3 == 0 )) && (( $i % 5 == 0 )); then echo "for-loop"
+                elif (( $i % 3 == 0 )); then echo "for"
+                elif (( $i % 5 == 0 )); then echo "loop"
+        else echo $i;
+        fi
+done
+
+
+################################
+echo
+sleep 2
+################################
+
+
+################
+# while loop-1 #
+################
+
+(( i=1 ));
+while (( i < 31 ));
+do
+        if { ( (( i % 5 == 0 )) && (( i % 3 == 0 )) ) } then echo "while-loop";
+        elif (( i % 3 == 0 )); then echo "while";
+        elif (( i % 5 == 0 )); then echo "loop";
+        else
+                echo "$i";
+        fi
+(( i++ ))
+done
+
+################################
+echo
+sleep 2
+################################
+
+################
+# while loop-2 #
+################
+
+#!/bin/bash
+(( i=1 ));
+while (( i<31 ));
+do
+        if (( $i % 3 == 0 )) && (( $i % 5 == 0 )); then echo "for-loop"
+        elif (( $i % 3 == 0 )); then echo "for"
+        elif (( $i % 5 == 0 )); then echo "loop"
+        else echo $i;
+        fi
+((i++))
+done
