51e99fb4b440f8257909362d04cc5f6116a34651
nozatech <albertpark5@gmail.com>
2016-07-08 09:31:04 -0700

    new

--- status_check.sh
+++ status_check.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# Check and put ok status if the RRS game service is running for Lobby 7504 & 7505 services
+# RRS Check and put ok status if the RRS game service is running for Lobby 7504 & 7505 services
+# Add to crontab -e "5 * * * * /root/bin/status.sh > /dev/null 2>&1 "
 
-if (( $(ps aux | grep ./RRServer_0328_7504 | wc -l) > 0  &&  $(ps aux | grep ./RRServer_0328_7505 | wc -l) > 0 )); then
+if (( $(ps aux | grep ./RRServer_0328_7504 | grep -v grep | wc -l) > 0  &&  $(ps aux | grep ./RRServer_0328_7505  grep -v grep || wc -l) > 0 )); then
         echo OK > /var/www/html/status/index.html;
-else rm /var/www/html/status/index.html > /dev/null 2>&1;
+else rm -f /var/www/html/status/index.html > /dev/null 2>&1;
 fi
 
+
+#!/bin/bash
+# PMF Httpd process check and put ok status
+# Add to crontab -e "5 * * * * /root/bin/status.sh > /dev/null 2>&1 "
+
+if (( $(ps aux | grep httpd | grep -v grep | wc -l) > 0  &&  $(ps aux | grep httpd| grep -v grep |wc -l) > 0 )); then
+        echo ok > /var/www/html/status/index.html;
+else rm -f /var/www/html/status/index.html > /dev/null 2>&1;
+fi
