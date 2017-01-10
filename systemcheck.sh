20a725d8424bc734341c6ec43256a559c415e924
nozatech <albertpark5@gmail.com>
2017-01-10 14:19:20 -0800

    api

--- systemcheck.sh
+++ systemcheck.sh
@@ -1,6 +1,37 @@
 #! /bin/bash
-# unset any variable which system may be using
 
+### Result should be same as below ###-------------------------
+#Internet:  Connected
+#Operating System Type :  GNU/Linux
+#OS Name : CentOS Linux
+#cpe:/o:centos:centos:7
+#OS Version : 7 (Core)
+#7
+#7
+#Architecture :  x86_64
+#Kernel Release :  3.10.0-327.36.3.el7.x86_64
+#Hostname :  i7
+#Internal IP :  10.100.5.155
+#External IP :  65.87.26.125
+#Name Servers :  namco.local 10.10.1.10
+#Logged In users :
+#apark    pts/0        2016-12-06 09:12 (10.100.5.190)
+#Ram Usages :
+#              total        used        free      shared  buff/cache   available
+#Mem:           7.6G        140M        7.3G        8.5M        147M        7.3G
+#Swap Usages :
+#              total        used        free      shared  buff/cache   available
+#Swap:          7.7G          0B        7.7G
+#Disk Usages :
+#Filesystem               Size  Used Avail Use% Mounted on
+#/dev/sda1                497M  167M  331M  34% /boot
+#Load Average :  average:0.05,0.03,
+#System Uptime Days/(HH:MM) :  46 min
+#----------------------------------------------------------------------------------
+
+
+
+# unset any variable which system may be using
 # clear the screen
 clear
 
@@ -15,28 +46,24 @@ do
         esac
 done
 
-if [[ ! -z $iopt ]]
-then
+#----------------------------------------------------------------------------------
+if [[ ! -z $iopt ]]; then
 {
 wd=$(pwd)
 basename "$(test -L "$0" && readlink "$0" || echo "$0")" > /tmp/scriptname
 scriptname=$(echo -e -n $wd/ && cat /tmp/scriptname)
-su -c "cp $scriptname /usr/bin/monitor" root && echo "Congratulations! Script Installed, now run monitor Command" || echo "Installation failed"
+su -c "cp $scriptname /usr/bin/monitor" root && echo "Script has Installed, run 'monitor' Command" || echo "Installation failed"
 }
 fi
-
-if [[ ! -z $vopt ]]
-then
+#----------------------------------------------------------------------------------
+if [[ ! -z $vopt ]]; then
 {
 echo -e "tecmint_monitor version 0.1\nDesigned by Tecmint.com\nReleased Under Apache 2.0 License"
 }
 fi
-
-if [[ $# -eq 0 ]]
-then
+#----------------------------------------------------------------------------------
+if [[ $# -eq 0 ]] ; then
 {
-
-
 # Define Variable tecreset
 tecreset=$(tput sgr0)
 
@@ -106,5 +133,6 @@ unset tecreset os architecture kernelrelease internalip externalip nameserver lo
 rm /tmp/osrelease /tmp/who /tmp/ramcache /tmp/diskusage
 }
 fi
+
 shift $(($OPTIND -1))
                                                                                                                                                                             117,19        Bot
