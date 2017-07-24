5531973c89c68908f98aab173cd5e9992057b632
nozatech <albertpark5@gmail.com>
2017-07-24 10:33:17 -0700

    update

--- status_check.sh
+++ status_check.sh
@@ -16,3 +16,40 @@ if (( $(ps aux | grep httpd | grep -v grep | wc -l) > 0  &&  $(ps aux | grep htt
         echo ok > /var/www/html/status/index.html;
 else rm -f /var/www/html/status/index.html > /dev/null 2>&1;
 fi
+
+
+
+
+
+#!/bin/bash
+
+### lobby 7505 ###
+
+RRSERVER="RRServer_7505"
+
+ps aux | grep -v grep | grep ./RRServer_0617_7505 > /dev/null 2>&1
+
+if [ $? != 0 ]
+   then
+		
+		/$RRSERVER/start-us-lobby-7505.sh
+fi
+
+##################
+### lobby 7504 ###
+##################
+
+RRSERVER="RRServer_7504"
+
+ps aux | grep -v grep | grep ./RRServer_0402_7504 >/dev/null 2>&1
+
+if [ $? != 0 ]
+   then
+		/$RRSERVER/start-us-lobby-7504.sh
+fi
+
+
+
+
+./RRServer_0417_7505 slave port 7510 mAddress 10.130.33.41 mPort 7505 publicAddress 128.199.203.10 maxConnections 500
+ start-us-s1.sh
